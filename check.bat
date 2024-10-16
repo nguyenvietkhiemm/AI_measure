c88098248c7a9449f225202bf3d3edc0aafb3497
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-10-16 19:02:05 +0700

    fixed2

--- check.bat
+++ check.bat
@@ -1,6 +1,18 @@
+@echo off
+echo Extracting requirements...
 pip freeze > requirements.txt
 
-@echo off
-echo extract the requirements...
-pip freeze > requirements.txt
+echo Checking for uncommitted changes...
+git status
+
+rem
+if not "%ERRORLEVEL%" == "0" (
+    echo No changes to commit.
+    pause
+    exit /b
+)
+
+git add .
+
+echo Preparing to push changes...
 pause
