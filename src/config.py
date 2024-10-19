01bdc593f5ccdcc468581ee3c391cd2d68cac728
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-10-19 17:55:47 +0700

    fix_19/10/2024_17:55:47,87

--- src/config.py
+++ src/config.py
@@ -1,9 +1,12 @@
-input_columns = ["Gender","Age"]
+# input_columns = ["height","weight","gender", "age", "form"]
 
 # output_columns = ['shoulder', 'sleeve',
 #        'neck', 'chest', 'waist', 'stomach', 'hip', 'front_jacket', 'biceps',
 #        'armhole', 'front_vest', 'back_length', 'crotch', 'pants_length',
 #        'thight', 'lower_thight', 'calves', 'leg_opening_width']
+
+
+input_columns = ["Gender","Age"]
 
 output_columns = ["HeadCircumference","ShoulderWidth","ChestWidth" ,"Belly" ,"Waist" ,"Hips" ,"ArmLength" ,"ShoulderToWaist" ,"WaistToKnee" ,"LegLength","TotalHeight"]
 
